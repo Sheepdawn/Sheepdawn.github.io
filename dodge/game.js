f21f783626d95ada0c4756729871319a2af67a76
Trevor B <tjb32@scasd.org>
2017-03-16 20:17:49 +0000

    progress

---
+++ dodge/game.js
@@ -0,0 +1,7 @@
+function setup() {
+    createCanvas(250, 250);
+}
+
+function draw() {
+    background(loadImage("https://tjb32.github.io/dodge/bg.jpg"));
+}
---
+++ geometric_pattern/main.js
@@ -0,0 +1,8 @@
+var Num_Circles=12;
+var CircleDiameter;
+function setup()  {
+    createCanvas(480,600);
+}
+function draw() {
+    ellipse(width/3,height/3,CircleDiameter,CircleDiameter);
+}
